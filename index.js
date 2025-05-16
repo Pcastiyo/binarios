33a899efc1de1d65a3f93dddb9220942b7e8b94b
Sergio <pitufo_bern@hotmail.com>
2025-05-16 00:26:12 -0300

    binarios

---
+++ index.js
@@ -0,0 +1,40 @@
+'use strict'
+//Clase 8 practica logica prodevs academy
+
+function BinarioADecimal(num) {
+  // tu codigo aca
+  /* 
+  imput "110"
+  output "6"
+  convertir el string en array
+  [1,1,0]
+  dar vuelta el array
+  [0,1,1];
+  recorrer el array usando la posicion y esa posicion que sea el factor por el que lo voy a hacer
+  multiplicar el resultado por el valor del elemento del array
+  sumar los resultados.
+  */
+
+  //recorrer el array con un for
+
+ 
+
+}
+
+function DecimalABinario(num) {
+  // tu codigo aca
+  /* dividir num entre dos hasta que llegue a 0
+ quedarme con el resultado entero sin  el decimal y el resto.
+ ir recordando los restos
+ cambiar el orden de los restos y agruparlos para devolverlos como string
+
+*/
+
+  
+}
+
+
+module.exports = {
+  BinarioADecimal,
+  DecimalABinario,
+}
